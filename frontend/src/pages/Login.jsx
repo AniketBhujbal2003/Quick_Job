c625d40b27f5e51c5a403404c22d602063b91392
Aniket <aniket@gmail.com>
2025-06-19 16:08:50 +0530

    add disabled to login button

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -81,7 +81,7 @@ const Login = () => {
                 <RiLock2Fill />
               </div>
             </div>
-            <button type="submit" >
+            <button type="submit" disabled={loading} >
               Login
             </button>
             <Link to={"/register"}>Register Now</Link>
